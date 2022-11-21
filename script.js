ba51c5d728e28f474d6159b08b208c1c4b55ed6d
Shushu9 <pokotilo.gleb1@gmail.com>
2022-11-21 20:37:57 +0300

    add tads and more music

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-console.log("Самооценка pt1:\n1. Вёрстка +10; \n2. При кликах по интерактивным элементам меняется изображение +10\n3. При кликах по интерактивным элементам меняется звук +10\n4. Активный в данный момент интерактивный элемент выделяется стилем +10\n5. Кнопка Play/Pause +20\n6. Очень высокое качество оформления приложения и/или дополнительный не предусмотренный в задании функционал, улучшающий качество приложения 0\n Итог: 60 баллов.");
+console.log("Самооценка pt1:\n1. Вёрстка +10; \n2. При кликах по интерактивным элементам меняется изображение +10\n3. При кликах по интерактивным элементам меняется звук +10\n4. Активный в данный момент интерактивный элемент выделяется стилем +10\n5. Кнопка Play/Pause +20\n6. Внизу табы переключающие жанр = дополнительный не предусмотренный в задании функционал, улучшающий качество приложения +10\n Итог: 70 баллов.");
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -9,7 +9,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const playBtn = document.querySelector('.play'),
         birdsBtns = document.querySelectorAll('.menu__item'),
         background = document.querySelector('main'),
-        logo = document.querySelector('.header__logo');
+        logo = document.querySelectorAll('.header__logo'),
+        tabs = document.querySelectorAll('.footer__tab'),
+        songsParent = document.querySelectorAll('.menu');
 
     let currentMelody = 'forest',
         isPlay = false;
@@ -50,17 +52,51 @@ window.addEventListener('DOMContentLoaded', () => {
             birdsBtns.forEach(item => item.classList.remove('active'));
             item.classList.add('active');
             background.style.backgroundImage = `url(../img/${currentMelody}.jpg)`
-            logo.classList.remove('active');
+            logo.forEach(item => item.classList.remove('active'));
         })
     })
 
 
-    logo.addEventListener('click', (i) => {
-        logo.classList.add('active');
-        currentMelody = 'forest';
-        playAudio(currentMelody);
-        birdsBtns.forEach(item => item.classList.remove('active'));
-        background.style.backgroundImage = `url(../img/${currentMelody}.jpg)`
+    logo.forEach(item => {
+        item.addEventListener('click', (i) => {
+            item.classList.add('active');
+            currentMelody = (logo[0].classList.contains('show')) ? 'forest' : 'cycle';
+            playAudio(currentMelody);
+            birdsBtns.forEach(item => item.classList.remove('active'));
+            background.style.backgroundImage = `url(../img/${currentMelody}.jpg)`
+        })
+    })
+
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', (el) => {
+            tabs.forEach(item => item.classList.remove('active'));
+            tab.classList.add('active');
+
+            playBtn.style.backgroundImage = "url(../svg/play.svg)";
+            isPlay = false;
+            audio.pause();
+
+            if (tab.classList.contains('footer__tab-skull')) {
+                currentMelody = 'cycle';
+                background.style.backgroundImage = `url(../img/${currentMelody}.jpg)`
+                songsParent.forEach(item => item.classList.remove('show'));
+                songsParent[1].classList.add('show');
+                logo[0].classList.remove('show');
+                logo[1].classList.add('show');
+                logo[0].classList.remove('active');
+                logo[1].classList.add('active');
+            } else {
+                currentMelody = 'forest';
+                background.style.backgroundImage = `url(../img/${currentMelody}.jpg)`
+                songsParent.forEach(item => item.classList.remove('show'));
+                songsParent[0].classList.add('show');
+                logo[1].classList.remove('show');
+                logo[0].classList.add('show');
+                logo[1].classList.remove('active');
+                logo[0].classList.add('active');
+            }
+        })
     })
 
 });
